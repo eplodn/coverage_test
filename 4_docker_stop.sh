8bad8df8fe01b77b483d0bc72c85ea6b38202ade
Efi Plotkin <eplotkin@drivenets.com>
2020-06-11 09:59:08 +0000

    finishing touches

--- 4_docker_stop.sh
+++ 4_docker_stop.sh
@@ -3,3 +3,5 @@
 docker exec eplodn_container pkill -SIGINT python
 
 sudo chown -R "$USER":"$USER" coverage
+
+sleep 2
--- 5_coverage.sh
+++ 5_coverage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd coverage
 python3.6 -m coverage combine
-python3.6 -m coverage xml
+python3.6 -m coverage report -m -i
 
